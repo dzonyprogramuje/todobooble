c4d3ff7739c2eb2975793b1f83f4ed18fcf5c0f1
dzonyprogramuje <dzonyprogramuje@gmail.com>
2020-06-25 18:21:28 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <>Start to development</>
+    <>Start to development - 2</>
   );
 }
 
